b666fcf9f339644964f79626026bb5a88954df34
yadnyesh-96 <yadnyneshdhangar@gmail.com>
2026-05-03 23:49:24 +0530

    node learning

--- 03-server/index-1.js
+++ 03-server/index-1.js
@@ -5,17 +5,19 @@ const myServer = http.createServer((req,res)=>{
     
     const myUrl = url.parse(req.url,true)
     console.log(myUrl)
+    console.log("PATH:", myUrl.pathname);
     switch (myUrl.pathname){
         case "/":
             res.end("Home");
             break;
         case "/about":
             const username = myUrl.query.myname;
+            const role = myUrl.query.role;
             const id = myUrl.query.id;
-            res.end(`Hi ${username} \n and userId :${id}`);
+            res.end(`Hi ${username} \n and userId :${id} \n Role ${role}` );
             break;
         case "/contact":
-            res.end("contact us");
+            res.end("Contact-us")
             break;
         default:
             res.end("404 Error !");
---
+++ 03-server/tempCodeRunnerFile.js
@@ -0,0 +1,3 @@
+const contNum = myUrl.query.no
+            // res.end("contact us");
+            // res.end(`Contact no ${contNum}`)
